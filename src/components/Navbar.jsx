0c9042691417f6d72b4d7cc368c17852b2cfc534
bryantlimm <142878856+bryantlimm@users.noreply.github.com>
2026-08-08 17:07:01 +0700

    fix directory

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import whiteLogo from './assets/White_Logo.png';
+import whiteLogo from "../assets/White_Logo.png";
 
 const links = [
   { to: '/', label: 'Home' },
